08e105ebaa913c16d0bf9ea45d9a6e4015367932
Annabel Schneider <aeschneider@umass.edu>
2019-03-24 17:18:52 -0400

    attempt

--- src/App01.jsx
+++ src/App01.jsx
@@ -35,3 +35,32 @@ class MyComponent extends React.Component {
 
 // This renders the JSX component inside the content node:
 ReactDOM.render(<MyComponent />, contentNode);
+
+import React, { Component } from "react";
+import { MDBBtn, MDBCollapse } from "mdbreact";
+
+class CollapsePage extends Component {
+
+render() {
+  return (
+      <>
+        <MDBBtn color="primary" onClick={this.toggleCollapse("basicCollapse")} style={{ marginBottom: "1rem" }}>
+          Toggle1
+        </MDBBtn>
+        <MDBBtn color="info" onClick={this.toggleCollapse("basicCollapse")} style={{ marginBottom: "1rem" }}>
+          Toggle2
+        </MDBBtn>
+        <MDBCollapse id="basicCollapse" isOpen={this.state.collapseID}>
+          <p>
+            Anim pariatur cliche reprehenderit, enim eiusmod high life
+            accusamus terry richardson ad squid. Nihil anim keffiyeh
+            helvetica, craft beer labore wes anderson cred nesciunt sapiente
+            ea proident.
+          </p>
+        </MDBCollapse>
+      </>
+    );
+  }
+}
+
+export default CollapsePage;
