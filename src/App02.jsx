2e5c8d5e772c19e16c2417bcfb46a397b2d825e8
Maristopher <matthewb1098@gmail.com>
2019-03-24 16:51:06 -0400

    phase2

--- src/App02.jsx
+++ src/App02.jsx
@@ -1,24 +1,131 @@
-// This is a place holder for the initial application state.
-const state = [
-
+const carts = [
 ];
 
-// This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
-class MyComponent extends React.Component {
+class CartFilter extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+const MenuRow = (props) => (
+  <tr>
+    <td>{props.cart.item}</td>
+  </tr>
+);
+
+
+function MenuTable(props) {
+  const CartRows = props.carts.map(cart => (
+    <CartRow key={cart.item} cart={cart} />
+  ));
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr>
+          <th>Menu</th>
+        </tr>
+      </thead>
+      <tbody>{CartRows}</tbody>
+    </table>
+  );
+}
+
+const CartRow = (props) => (
+  <tr>
+    <td>{props.cart.item}</td>
+  </tr>
+);
+
+
+function CartTable(props) {
+  const CartRows = props.carts.map(cart => (
+    <CartRow key={cart.item} cart={cart} />
+  ));
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr>
+          <th>Your Cart</th>
+        </tr>
+      </thead>
+      <tbody>{CartRows}</tbody>
+    </table>
+  );
+}
+
+class CartAdd extends React.Component {
   constructor() {
     super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let form = document.forms.cartAdd;
+    this.props.createCart({
+      item: form.item.value,
+      status: 'New',
+      created: new Date(),
+    });
+    // Clear the form for the next input.
+    form.item.value = '';
+  }
+
+  render() {
+    return (
+      <div>
+        <form name="cartAdd" onSubmit={this.handleSubmit}>
+          <input type="text" name="item" placeholder="Item" />
+          <button>Add</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+class CartList extends React.Component {
+  constructor() {
+    super();
+    this.state = { carts: [] };
+
+    this.createCart = this.createCart.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    setTimeout(() => {
+      this.setState({
+        carts: carts
+      });
+    }, 500);
+  }
+
+  createCart(newCart) {
+    const newcarts = this.state.carts.slice();
+    newcarts.push(newCart);
+    this.setState({ carts: newcarts });
   }
 
   render() {
     return (
       <div>
         <h1>Place an Order</h1>
+        <CartFilter />
+        <hr />
+        <MenuTable carts={this.state.carts} />
+        <hr />   
+        <CartAdd createCart={this.createCart} />
+        <CartTable carts={this.state.carts} />
+        <hr />
       </div>
     );
   }
 }
 
 // This renders the JSX component inside the content node:
-ReactDOM.render(<MyComponent />, contentNode);
+ReactDOM.render(<CartList />, contentNode);
