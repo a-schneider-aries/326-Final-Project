299f6f15135d259f560f440c1c0e804f4a78c1a9
Annabel Schneider <aeschneider@umass.edu>
2019-03-24 16:54:31 -0400

    dining halls

--- src/App01.jsx
+++ src/App01.jsx
@@ -1,3 +1,5 @@
+import { pbkdf2 } from "crypto";
+
 // This is a place holder for the initial application state.
 const state = [
 
@@ -15,6 +17,17 @@ class MyComponent extends React.Component {
     return (
       <div>
         <h1>Dining Locations and Their Food</h1>
+        <p1>Baby Berk 1</p1>
+        <p2>Baby Berk 2</p2>
+        <p3>Berkshire Dining Common </p3>
+        <p4>Blue Wall</p4>
+        <p5>Franklin Dining Common</p5>
+        <p6>Greenough Subs</p6>
+        <p7>Hampshire Dining Common</p7>
+        <p8>Procrastination Station</p8>
+        <p9>Roots Cafe</p9>
+        <p10>Sweets and More</p10>
+        <p11>Worcester Dining Common</p11>
       </div>
     );
   }
