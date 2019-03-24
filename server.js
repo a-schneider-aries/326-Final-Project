c433169f52701feddfc2b83221d825f570fa332f
Annabel Schneider <aeschneider@umass.edu>
2019-03-24 16:44:56 -0400

    attempt

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static('static'));
 
-app.listen(3000, function () {
-    console.log('App started on port 3000');
+app.listen(8080, function () {
+    console.log('App started on port 8080');
 });
