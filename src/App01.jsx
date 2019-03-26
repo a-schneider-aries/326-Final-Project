a14859ecb676327592c4d1c761fdb08797d1acbc
Maristopher <matthewb1098@gmail.com>
2019-03-26 15:05:59 -0400

    f

--- src/App01.jsx
+++ src/App01.jsx
@@ -1,76 +1,77 @@
-//import { pbkdf2 } from "crypto";
+// //import { pbkdf2 } from "crypto";
 
-// This is a place holder for the initial application state.
-const state = [
+// // This is a place holder for the initial application state.
+// const state = [
 
-];
+// ];
 
-// This grabs the DOM element to be used to mount React components.
-var contentNode = document.getElementById("contents");
+// // This grabs the DOM element to be used to mount React components.
+// var contentNode = document.getElementById("contents");
 
-class Example extends Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = { collapse: false };
-  }
 
-  toggle() {
-    this.setState(state => ({ collapse: !state.collapse }));
-  }
+// // class Example extends Component {
+// //   constructor(props) {
+// //     super(props);
+// //     this.toggle = this.toggle.bind(this);
+// //     this.state = { collapse: false };
+// //   }
 
-  render() {
-    return (
-      <div>
-        <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
-        <Collapse isOpen={this.state.collapse}>
-          <Card>
-            <CardBody>
-            Anim pariatur cliche reprehenderit,
-             enim eiusmod high life accusamus terry richardson ad squid. Nihil
-             anim keffiyeh helvetica, craft beer labore wes anderson cred
-             nesciunt sapiente ea proident.
-            </CardBody>
-          </Card>
-        </Collapse>
-      </div>
-    );
-  }
-}
+// //   toggle() {
+// //     this.setState(state => ({ collapse: !state.collapse }));
+// //   }
 
-class MyComponent extends React.Component {
-  constructor() {
-    super();
-  }
+// //   render() {
+// //     return (
+// //       <div>
+// //         <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+// //         <Collapse isOpen={this.state.collapse}>
+// //           <Card>
+// //             <CardBody>
+// //             Anim pariatur cliche reprehenderit,
+// //              enim eiusmod high life accusamus terry richardson ad squid. Nihil
+// //              anim keffiyeh helvetica, craft beer labore wes anderson cred
+// //              nesciunt sapiente ea proident.
+// //             </CardBody>
+// //           </Card>
+// //         </Collapse>
+// //       </div>
+// //     );
+// //   }
+// // }
 
-  render() {
-    return (
-      <div>
-        <h1>Dining Locations and Their Food</h1>
-        <div><h1>Bamboo</h1></div>
-        <p>Authentic dim sum selections and hot plates.</p>
-        <div><h1>Deli Delish</h1></div>
-        <p>Hot and cold grinders featuring house-roasted turkey and roast beef.</p>
-        <div><h1>Green Fields</h1></div>
-        <p>Freshly tossed salads made to-order with locally sourced ingredients.</p>
-        <div><h1>The Grill</h1></div>
-        <p>Classic American fare ranging from burgers to chicken tenders to milkshakes.</p>
-        <div><h1>Paciugo</h1></div>
-        <p>A selection of smooth, all-natural gelato.</p>
-        <div><h1>Star Ginger</h1></div>
-        <p>Pho noodle bowls, rice bowls, and authentic Korean bibimbap.</p>
-        <div><h1>Tamales</h1></div>
-        <p>Burritos, bowls, and salads made to-order.</p>
-        <div><h1>Tavola</h1></div>
-        <p>Classic Mediterranean fare including hummus bowls, pizza, and pasta.</p>
-        <div><h1>Wasabi</h1></div>
-        <p>Sushi made with sustainably sourced fish, miso soup, and the like.</p>
-        <div><h1>Yum! Bakery</h1></div>
-        <p>Freshly baked goods including cookies, cakes, and French macaroons.</p>
-      </div>
-    );
-  }
-}
+// class MyComponent extends React.Component {
+//   constructor() {
+//     super();
+//   }
 
-// This renders the JSX component inside the content node:
-ReactDOM.render(<MyComponent />, contentNode);
+//   render() {
+//     return (
+//       <div>
+//         <h1>Dining Locations and Their Food</h1>
+//         <div><h1>Bamboo</h1></div>
+//         <p>Authentic dim sum selections and hot plates.</p>
+//         <div><h1>Deli Delish</h1></div>
+//         <p>Hot and cold grinders featuring house-roasted turkey and roast beef.</p>
+//         <div><h1>Green Fields</h1></div>
+//         <p>Freshly tossed salads made to-order with locally sourced ingredients.</p>
+//         <div><h1>The Grill</h1></div>
+//         <p>Classic American fare ranging from burgers to chicken tenders to milkshakes.</p>
+//         <div><h1>Paciugo</h1></div>
+//         <p>A selection of smooth, all-natural gelato.</p>
+//         <div><h1>Star Ginger</h1></div>
+//         <p>Pho noodle bowls, rice bowls, and authentic Korean bibimbap.</p>
+//         <div><h1>Tamales</h1></div>
+//         <p>Burritos, bowls, and salads made to-order.</p>
+//         <div><h1>Tavola</h1></div>
+//         <p>Classic Mediterranean fare including hummus bowls, pizza, and pasta.</p>
+//         <div><h1>Wasabi</h1></div>
+//         <p>Sushi made with sustainably sourced fish, miso soup, and the like.</p>
+//         <div><h1>Yum! Bakery</h1></div>
+//         <p>Freshly baked goods including cookies, cakes, and French macaroons.</p>
+//       </div>
+//     );
+//   }
+// }
+
+// // This renders the JSX component inside the content node:
+// ReactDOM.render(<MyComponent />, contentNode);
--- static/App01.js
+++ static/App01.js
@@ -1,240 +1,78 @@
+// //import { pbkdf2 } from "crypto";
+
+// // This is a place holder for the initial application state.
+// const state = [
+
+// ];
+
+// // This grabs the DOM element to be used to mount React components.
+// var contentNode = document.getElementById("contents");
+
+
+// // class Example extends Component {
+// //   constructor(props) {
+// //     super(props);
+// //     this.toggle = this.toggle.bind(this);
+// //     this.state = { collapse: false };
+// //   }
+
+// //   toggle() {
+// //     this.setState(state => ({ collapse: !state.collapse }));
+// //   }
+
+// //   render() {
+// //     return (
+// //       <div>
+// //         <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+// //         <Collapse isOpen={this.state.collapse}>
+// //           <Card>
+// //             <CardBody>
+// //             Anim pariatur cliche reprehenderit,
+// //              enim eiusmod high life accusamus terry richardson ad squid. Nihil
+// //              anim keffiyeh helvetica, craft beer labore wes anderson cred
+// //              nesciunt sapiente ea proident.
+// //             </CardBody>
+// //           </Card>
+// //         </Collapse>
+// //       </div>
+// //     );
+// //   }
+// // }
+
+// class MyComponent extends React.Component {
+//   constructor() {
+//     super();
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Dining Locations and Their Food</h1>
+//         <div><h1>Bamboo</h1></div>
+//         <p>Authentic dim sum selections and hot plates.</p>
+//         <div><h1>Deli Delish</h1></div>
+//         <p>Hot and cold grinders featuring house-roasted turkey and roast beef.</p>
+//         <div><h1>Green Fields</h1></div>
+//         <p>Freshly tossed salads made to-order with locally sourced ingredients.</p>
+//         <div><h1>The Grill</h1></div>
+//         <p>Classic American fare ranging from burgers to chicken tenders to milkshakes.</p>
+//         <div><h1>Paciugo</h1></div>
+//         <p>A selection of smooth, all-natural gelato.</p>
+//         <div><h1>Star Ginger</h1></div>
+//         <p>Pho noodle bowls, rice bowls, and authentic Korean bibimbap.</p>
+//         <div><h1>Tamales</h1></div>
+//         <p>Burritos, bowls, and salads made to-order.</p>
+//         <div><h1>Tavola</h1></div>
+//         <p>Classic Mediterranean fare including hummus bowls, pizza, and pasta.</p>
+//         <div><h1>Wasabi</h1></div>
+//         <p>Sushi made with sustainably sourced fish, miso soup, and the like.</p>
+//         <div><h1>Yum! Bakery</h1></div>
+//         <p>Freshly baked goods including cookies, cakes, and French macaroons.</p>
+//       </div>
+//     );
+//   }
+// }
+
+// // This renders the JSX component inside the content node:
+// ReactDOM.render(<MyComponent />, contentNode);
 "use strict";
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-//import { pbkdf2 } from "crypto";
-
-// This is a place holder for the initial application state.
-var state = [];
-
-// This grabs the DOM element to be used to mount React components.
-var contentNode = document.getElementById("contents");
-
-var Example = function (_Component) {
-  _inherits(Example, _Component);
-
-  function Example(props) {
-    _classCallCheck(this, Example);
-
-    var _this = _possibleConstructorReturn(this, (Example.__proto__ || Object.getPrototypeOf(Example)).call(this, props));
-
-    _this.toggle = _this.toggle.bind(_this);
-    _this.state = { collapse: false };
-    return _this;
-  }
-
-  _createClass(Example, [{
-    key: "toggle",
-    value: function toggle() {
-      this.setState(function (state) {
-        return { collapse: !state.collapse };
-      });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        React.createElement(
-          Button,
-          { color: "primary", onClick: this.toggle, style: { marginBottom: '1rem' } },
-          "Toggle"
-        ),
-        React.createElement(
-          Collapse,
-          { isOpen: this.state.collapse },
-          React.createElement(
-            Card,
-            null,
-            React.createElement(
-              CardBody,
-              null,
-              "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
-            )
-          )
-        )
-      );
-    }
-  }]);
-
-  return Example;
-}(Component);
-
-var MyComponent = function (_React$Component) {
-  _inherits(MyComponent, _React$Component);
-
-  function MyComponent() {
-    _classCallCheck(this, MyComponent);
-
-    return _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this));
-  }
-
-  _createClass(MyComponent, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        React.createElement(
-          "h1",
-          null,
-          "Dining Locations and Their Food"
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Bamboo"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Authentic dim sum selections and hot plates."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Deli Delish"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Hot and cold grinders featuring house-roasted turkey and roast beef."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Green Fields"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Freshly tossed salads made to-order with locally sourced ingredients."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "The Grill"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Classic American fare ranging from burgers to chicken tenders to milkshakes."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Paciugo"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "A selection of smooth, all-natural gelato."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Star Ginger"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Pho noodle bowls, rice bowls, and authentic Korean bibimbap."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Tamales"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Burritos, bowls, and salads made to-order."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Tavola"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Classic Mediterranean fare including hummus bowls, pizza, and pasta."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Wasabi"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Sushi made with sustainably sourced fish, miso soup, and the like."
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "h1",
-            null,
-            "Yum! Bakery"
-          )
-        ),
-        React.createElement(
-          "p",
-          null,
-          "Freshly baked goods including cookies, cakes, and French macaroons."
-        )
-      );
-    }
-  }]);
-
-  return MyComponent;
-}(React.Component);
-
-// This renders the JSX component inside the content node:
-
-
-ReactDOM.render(React.createElement(MyComponent, null), contentNode);
