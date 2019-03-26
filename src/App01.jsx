d1063506de17e125f6591149ce8f3f8523a39ae8
Annabel Schneider <aeschneider@umass.edu>
2019-03-26 16:21:46 -0400

    update menu

--- src/App01.jsx
+++ src/App01.jsx
@@ -8,40 +8,103 @@ const state = [
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
-class MyComponent extends React.Component {
-  constructor() {
-    super();
-  }
-
+class Bamboo extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Dining Locations and Their Food</h1>
-        <div>Bamboo</div>
-        <p1>    Authentic dim sum selections and hot plates.</p1>
-        <div>Deli Delish</div>
-        <p2>    Hot and cold grinders featuring house-roasted turkey and roast beef.</p2>
-        <div>Green Fields</div>
-        <p3>    Freshly tossed salads made to-order with locally sourced ingredients.</p3>
-        <div>The Grill</div>
-        <p4>    Classic American fare ranging from burgers to chicken tenders to milkshakes.</p4>
-        <div>Paciugo</div>
-        <p5>    A selection of smooth, all-natural gelato.</p5>
-        <div>Star Ginger</div>
-        <p6>    Pho noodle bowls, rice bowls, and authentic Korean bibimbap.</p6>
-        <div>Tamales</div>
-        <p7>    Burritos, bowls, and salads made to-order.</p7>
-        <div>Tavola</div>
-        <p8>    Classic Mediterranean fare including hummus bowls, pizza, and pasta.</p8>
-        <div>Wasabi</div>
-        <p9>    Sushi made with sustainably sourced fish, miso soup, and the like.</p9>
-        <div>Yum! Bakery</div>
-        <p10>   Freshly baked goods including cookies, cakes, and French macaroons.</p10>
-
-      </div>
-    );
+  return (
+  <div>Bamboo</div>
+  )
   }
-}
-
-// This renders the JSX component inside the content node:
-ReactDOM.render(<MyComponent />, contentNode);
+ }
+ class DeliDelish extends React.Component {
+  render() {
+  return (
+  <div>Deli Delish</div>
+  )
+  }
+ }
+ 
+ class GreenFields extends React.Component {
+  render() {
+  return (
+  <div>Green Fields</div>
+  )
+  }
+ }
+ class Grill extends React.Component {
+  render() {
+  return (
+  <div>The Grill</div>
+  )
+  }
+ }
+ class Paciugo extends React.Component {
+  render() {
+  return (
+  <div>Paciugo</div>
+  )
+  }
+ }
+ class StarGinger extends React.Component {
+  render() {
+  return (
+  <div>Star Ginger</div>
+  )
+  }
+ }
+ class Tamales extends React.Component {
+  render() {
+  return (
+  <div>Tamales</div>
+  )
+  }
+ }
+ class Tavola extends React.Component {
+  render() {
+  return (
+  <div>Tavola</div>
+  )
+  }
+ }
+ class Wasabi extends React.Component {
+  render() {
+  return (
+  <div>Wasabi</div>
+  )
+  }
+ }
+ class Yum extends React.Component {
+  render() {
+  return (
+  <div>Yum! Bakery</div>
+  )
+  }
+ }
+ class Menu extends React.Component {
+  render() {
+  return (
+  <div>
+  <h1>Dining Locations</h1>
+  <Bamboo />
+  <hr />
+  <DeliDelish />
+  <hr />
+  <GreenFields />
+  <hr />
+  <Grill />
+  <hr />
+  <Paciugo />
+  <hr />
+  <StarGinger />
+  <hr />
+  <Tamales />
+  <hr />
+  <Tavola />
+  <hr />
+  <Wasabi />
+  <hr />
+  <Yum />
+  </div>
+  );
+  }
+ }
+ ReactDOM.render(<Menu />, contentNode); 
--- static/App01.js
+++ static/App01.js
@@ -16,16 +16,246 @@ var state = [];
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
-var MyComponent = function (_React$Component) {
-  _inherits(MyComponent, _React$Component);
+var Bamboo = function (_React$Component) {
+  _inherits(Bamboo, _React$Component);
 
-  function MyComponent() {
-    _classCallCheck(this, MyComponent);
+  function Bamboo() {
+    _classCallCheck(this, Bamboo);
 
-    return _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this));
+    return _possibleConstructorReturn(this, (Bamboo.__proto__ || Object.getPrototypeOf(Bamboo)).apply(this, arguments));
   }
 
-  _createClass(MyComponent, [{
+  _createClass(Bamboo, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Bamboo"
+      );
+    }
+  }]);
+
+  return Bamboo;
+}(React.Component);
+
+var DeliDelish = function (_React$Component2) {
+  _inherits(DeliDelish, _React$Component2);
+
+  function DeliDelish() {
+    _classCallCheck(this, DeliDelish);
+
+    return _possibleConstructorReturn(this, (DeliDelish.__proto__ || Object.getPrototypeOf(DeliDelish)).apply(this, arguments));
+  }
+
+  _createClass(DeliDelish, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Deli Delish"
+      );
+    }
+  }]);
+
+  return DeliDelish;
+}(React.Component);
+
+var GreenFields = function (_React$Component3) {
+  _inherits(GreenFields, _React$Component3);
+
+  function GreenFields() {
+    _classCallCheck(this, GreenFields);
+
+    return _possibleConstructorReturn(this, (GreenFields.__proto__ || Object.getPrototypeOf(GreenFields)).apply(this, arguments));
+  }
+
+  _createClass(GreenFields, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Green Fields"
+      );
+    }
+  }]);
+
+  return GreenFields;
+}(React.Component);
+
+var Grill = function (_React$Component4) {
+  _inherits(Grill, _React$Component4);
+
+  function Grill() {
+    _classCallCheck(this, Grill);
+
+    return _possibleConstructorReturn(this, (Grill.__proto__ || Object.getPrototypeOf(Grill)).apply(this, arguments));
+  }
+
+  _createClass(Grill, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "The Grill"
+      );
+    }
+  }]);
+
+  return Grill;
+}(React.Component);
+
+var Paciugo = function (_React$Component5) {
+  _inherits(Paciugo, _React$Component5);
+
+  function Paciugo() {
+    _classCallCheck(this, Paciugo);
+
+    return _possibleConstructorReturn(this, (Paciugo.__proto__ || Object.getPrototypeOf(Paciugo)).apply(this, arguments));
+  }
+
+  _createClass(Paciugo, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Paciugo"
+      );
+    }
+  }]);
+
+  return Paciugo;
+}(React.Component);
+
+var StarGinger = function (_React$Component6) {
+  _inherits(StarGinger, _React$Component6);
+
+  function StarGinger() {
+    _classCallCheck(this, StarGinger);
+
+    return _possibleConstructorReturn(this, (StarGinger.__proto__ || Object.getPrototypeOf(StarGinger)).apply(this, arguments));
+  }
+
+  _createClass(StarGinger, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Star Ginger"
+      );
+    }
+  }]);
+
+  return StarGinger;
+}(React.Component);
+
+var Tamales = function (_React$Component7) {
+  _inherits(Tamales, _React$Component7);
+
+  function Tamales() {
+    _classCallCheck(this, Tamales);
+
+    return _possibleConstructorReturn(this, (Tamales.__proto__ || Object.getPrototypeOf(Tamales)).apply(this, arguments));
+  }
+
+  _createClass(Tamales, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Tamales"
+      );
+    }
+  }]);
+
+  return Tamales;
+}(React.Component);
+
+var Tavola = function (_React$Component8) {
+  _inherits(Tavola, _React$Component8);
+
+  function Tavola() {
+    _classCallCheck(this, Tavola);
+
+    return _possibleConstructorReturn(this, (Tavola.__proto__ || Object.getPrototypeOf(Tavola)).apply(this, arguments));
+  }
+
+  _createClass(Tavola, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Tavola"
+      );
+    }
+  }]);
+
+  return Tavola;
+}(React.Component);
+
+var Wasabi = function (_React$Component9) {
+  _inherits(Wasabi, _React$Component9);
+
+  function Wasabi() {
+    _classCallCheck(this, Wasabi);
+
+    return _possibleConstructorReturn(this, (Wasabi.__proto__ || Object.getPrototypeOf(Wasabi)).apply(this, arguments));
+  }
+
+  _createClass(Wasabi, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Wasabi"
+      );
+    }
+  }]);
+
+  return Wasabi;
+}(React.Component);
+
+var Yum = function (_React$Component10) {
+  _inherits(Yum, _React$Component10);
+
+  function Yum() {
+    _classCallCheck(this, Yum);
+
+    return _possibleConstructorReturn(this, (Yum.__proto__ || Object.getPrototypeOf(Yum)).apply(this, arguments));
+  }
+
+  _createClass(Yum, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "Yum! Bakery"
+      );
+    }
+  }]);
+
+  return Yum;
+}(React.Component);
+
+var Menu = function (_React$Component11) {
+  _inherits(Menu, _React$Component11);
+
+  function Menu() {
+    _classCallCheck(this, Menu);
+
+    return _possibleConstructorReturn(this, (Menu.__proto__ || Object.getPrototypeOf(Menu)).apply(this, arguments));
+  }
+
+  _createClass(Menu, [{
     key: "render",
     value: function render() {
       return React.createElement(
@@ -34,116 +264,32 @@ var MyComponent = function (_React$Component) {
         React.createElement(
           "h1",
           null,
-          "Dining Locations and Their Food"
+          "Dining Locations"
         ),
-        React.createElement(
-          "div",
-          null,
-          "Bamboo"
-        ),
-        React.createElement(
-          "p1",
-          null,
-          "    Authentic dim sum selections and hot plates."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "Deli Delish"
-        ),
-        React.createElement(
-          "p2",
-          null,
-          "    Hot and cold grinders featuring house-roasted turkey and roast beef."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "Green Fields"
-        ),
-        React.createElement(
-          "p3",
-          null,
-          "    Freshly tossed salads made to-order with locally sourced ingredients."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "The Grill"
-        ),
-        React.createElement(
-          "p4",
-          null,
-          "    Classic American fare ranging from burgers to chicken tenders to milkshakes."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "Paciugo"
-        ),
-        React.createElement(
-          "p5",
-          null,
-          "    A selection of smooth, all-natural gelato."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "Star Ginger"
-        ),
-        React.createElement(
-          "p6",
-          null,
-          "    Pho noodle bowls, rice bowls, and authentic Korean bibimbap."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "Tamales"
-        ),
-        React.createElement(
-          "p7",
-          null,
-          "    Burritos, bowls, and salads made to-order."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "Tavola"
-        ),
-        React.createElement(
-          "p8",
-          null,
-          "    Classic Mediterranean fare including hummus bowls, pizza, and pasta."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "Wasabi"
-        ),
-        React.createElement(
-          "p9",
-          null,
-          "    Sushi made with sustainably sourced fish, miso soup, and the like."
-        ),
-        React.createElement(
-          "div",
-          null,
-          "Yum! Bakery"
-        ),
-        React.createElement(
-          "p10",
-          null,
-          "   Freshly baked goods including cookies, cakes, and French macaroons."
-        )
+        React.createElement(Bamboo, null),
+        React.createElement("hr", null),
+        React.createElement(DeliDelish, null),
+        React.createElement("hr", null),
+        React.createElement(GreenFields, null),
+        React.createElement("hr", null),
+        React.createElement(Grill, null),
+        React.createElement("hr", null),
+        React.createElement(Paciugo, null),
+        React.createElement("hr", null),
+        React.createElement(StarGinger, null),
+        React.createElement("hr", null),
+        React.createElement(Tamales, null),
+        React.createElement("hr", null),
+        React.createElement(Tavola, null),
+        React.createElement("hr", null),
+        React.createElement(Wasabi, null),
+        React.createElement("hr", null),
+        React.createElement(Yum, null)
       );
     }
   }]);
 
-  return MyComponent;
+  return Menu;
 }(React.Component);
 
-// This renders the JSX component inside the content node:
-
-
-ReactDOM.render(React.createElement(MyComponent, null), contentNode);
+ReactDOM.render(React.createElement(Menu, null), contentNode);
