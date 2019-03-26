49e13e5a7a5f30c2eb0f7ca04348227bf6cbeec9
System Administrator <root@1x-nat-vl932-172-30-196-5.wireless.umass.edu>
2019-03-26 15:59:18 -0400

    yoooooooo

--- src/App01.jsx
+++ src/App01.jsx
@@ -8,47 +8,15 @@ const state = [
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
-class Example extends Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = { collapse: false };
-  }
-
-  toggle() {
-    this.setState(state => ({ collapse: !state.collapse }));
-  }
-
-  render() {
-    return (
-      <div>
-        <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
-        <Collapse isOpen={this.state.collapse}>
-          <Card>
-            <CardBody>
-            Anim pariatur cliche reprehenderit,
-             enim eiusmod high life accusamus terry richardson ad squid. Nihil
-             anim keffiyeh helvetica, craft beer labore wes anderson cred
-             nesciunt sapiente ea proident.
-            </CardBody>
-          </Card>
-        </Collapse>
-      </div>
-    );
-  }
-}
-
 class MyComponent extends React.Component {
   constructor() {
     super();
   }
 
-
+  
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <button class="collapsible">Open Collapsible</button>
 
         <h1 class = "content">Dining Locations and Their Food</h1>
         <div>Bamboo</div>
@@ -72,49 +40,10 @@ class MyComponent extends React.Component {
         <div>Yum! Bakery</div>
         <p10>Freshly baked goods including cookies, cakes, and French macaroons.</p10>
 
-=======
-        <h1>Dining Locations and Their Food</h1>
-        <div><h1>Bamboo</h1></div>
-        <p>Authentic dim sum selections and hot plates.</p>
-        <div><h1>Deli Delish</h1></div>
-        <p>Hot and cold grinders featuring house-roasted turkey and roast beef.</p>
-        <div><h1>Green Fields</h1></div>
-        <p>Freshly tossed salads made to-order with locally sourced ingredients.</p>
-        <div><h1>The Grill</h1></div>
-        <p>Classic American fare ranging from burgers to chicken tenders to milkshakes.</p>
-        <div><h1>Paciugo</h1></div>
-        <p>A selection of smooth, all-natural gelato.</p>
-        <div><h1>Star Ginger</h1></div>
-        <p>Pho noodle bowls, rice bowls, and authentic Korean bibimbap.</p>
-        <div><h1>Tamales</h1></div>
-        <p>Burritos, bowls, and salads made to-order.</p>
-        <div><h1>Tavola</h1></div>
-        <p>Classic Mediterranean fare including hummus bowls, pizza, and pasta.</p>
-        <div><h1>Wasabi</h1></div>
-        <p>Sushi made with sustainably sourced fish, miso soup, and the like.</p>
-        <div><h1>Yum! Bakery</h1></div>
-        <p>Freshly baked goods including cookies, cakes, and French macaroons.</p>
->>>>>>> 9e08913a791103a4387dbbde2d3fa784012d8333
       </div>
     );
   }
 }
-// added code 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-// added code 
 
 // This renders the JSX component inside the content node:
 ReactDOM.render(<MyComponent />, contentNode);
--- static/App01.js
+++ static/App01.js
@@ -16,57 +16,6 @@ var state = [];
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
-var Example = function (_Component) {
-  _inherits(Example, _Component);
-
-  function Example(props) {
-    _classCallCheck(this, Example);
-
-    var _this = _possibleConstructorReturn(this, (Example.__proto__ || Object.getPrototypeOf(Example)).call(this, props));
-
-    _this.toggle = _this.toggle.bind(_this);
-    _this.state = { collapse: false };
-    return _this;
-  }
-
-  _createClass(Example, [{
-    key: "toggle",
-    value: function toggle() {
-      this.setState(function (state) {
-        return { collapse: !state.collapse };
-      });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        React.createElement(
-          Button,
-          { color: "primary", onClick: this.toggle, style: { marginBottom: '1rem' } },
-          "Toggle"
-        ),
-        React.createElement(
-          Collapse,
-          { isOpen: this.state.collapse },
-          React.createElement(
-            Card,
-            null,
-            React.createElement(
-              CardBody,
-              null,
-              "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."
-            )
-          )
-        )
-      );
-    }
-  }]);
-
-  return Example;
-}(Component);
-
 var MyComponent = function (_React$Component) {
   _inherits(MyComponent, _React$Component);
 
@@ -83,11 +32,6 @@ var MyComponent = function (_React$Component) {
         "div",
         null,
         React.createElement(
-          "button",
-          { "class": "collapsible" },
-          "Open Collapsible"
-        ),
-        React.createElement(
           "h1",
           { "class": "content" },
           "Dining Locations and Their Food"
@@ -95,140 +39,100 @@ var MyComponent = function (_React$Component) {
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Bamboo"
-          )
+          "Bamboo"
         ),
         React.createElement(
-          "p",
+          "p1",
           null,
           "Authentic dim sum selections and hot plates."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Deli Delish"
-          )
+          "Deli Delish"
         ),
         React.createElement(
-          "p",
+          "p2",
           null,
           "Hot and cold grinders featuring house-roasted turkey and roast beef."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Green Fields"
-          )
+          "Green Fields"
         ),
         React.createElement(
-          "p",
+          "p3",
           null,
           "Freshly tossed salads made to-order with locally sourced ingredients."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "The Grill"
-          )
+          "The Grill"
         ),
         React.createElement(
-          "p",
+          "p4",
           null,
           "Classic American fare ranging from burgers to chicken tenders to milkshakes."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Paciugo"
-          )
+          "Paciugo"
         ),
         React.createElement(
-          "p",
+          "p5",
           null,
           "A selection of smooth, all-natural gelato."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Star Ginger"
-          )
+          "Star Ginger"
         ),
         React.createElement(
-          "p",
+          "p6",
           null,
           "Pho noodle bowls, rice bowls, and authentic Korean bibimbap."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Tamales"
-          )
+          "Tamales"
         ),
         React.createElement(
-          "p",
+          "p7",
           null,
           "Burritos, bowls, and salads made to-order."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Tavola"
-          )
+          "Tavola"
         ),
         React.createElement(
-          "p",
+          "p8",
           null,
           "Classic Mediterranean fare including hummus bowls, pizza, and pasta."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Wasabi"
-          )
+          "Wasabi"
         ),
         React.createElement(
-          "p",
+          "p9",
           null,
           "Sushi made with sustainably sourced fish, miso soup, and the like."
         ),
         React.createElement(
           "div",
           null,
-          React.createElement(
-            "h1",
-            null,
-            "Yum! Bakery"
-          )
+          "Yum! Bakery"
         ),
         React.createElement(
-          "p",
+          "p10",
           null,
           "Freshly baked goods including cookies, cakes, and French macaroons."
         )
@@ -238,24 +142,8 @@ var MyComponent = function (_React$Component) {
 
   return MyComponent;
 }(React.Component);
-// added code 
-
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-// added code 
 
 // This renders the JSX component inside the content node:
+
+
 ReactDOM.render(React.createElement(MyComponent, null), contentNode);
