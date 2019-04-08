716f03a6f1ca2f8260d01fa3f37c218bfb33d039
Tiffany Lee <tslee@umass.edu>
2019-04-08 18:00:50 -0400

    server

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const body_parser = require('body-parser')
-const mongo = require('mongodb').MongoClient
 const express = require('express');
-const mongodb = require('mongodb');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -18,14 +16,10 @@ MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(conne
   db = connection.db('menu');
   app.listen(8080, () => {
     console.log('App started on port 8080');
-<<<<<<< HEAD
 });
 
 app.use(express.body_parser)
 app.use(express.mongo)
-=======
-  });
-}).catch(error => {
+  }).catch(error => {
   console.log('ERROR:', error);
 });
->>>>>>> 8331e3e778cf6e5d7c60966a7dc81f3d418a234e
